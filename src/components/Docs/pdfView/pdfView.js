5a16a4f47665b3d9beb0ea3671f553ba4886f884
MaxusWIN <newsfoxxxx@googlemail.com>
2020-11-12 01:25:32 +0100

    working pdf on Metadata & upload pdf on File Modal

--- src/components/Docs/pdfView/pdfView.js
+++ src/components/Docs/pdfView/pdfView.js
@@ -12,9 +12,7 @@ export default class PdfView extends Component {
   state = {
      numPages: null,
      pageNumber: 1,
-     activeNote: "https://firebasestorage.googleapis.com/v0/b/reactsearch-d67f0.appspot.com/o/chat%2Fpublic%2F684df0a1-9d3d-49be-9e89-3d2ddfd905cb.jpg?alt=media&token=e908b15d-f3ef-47c3-91cb-15548f25e31f"
-
-    };
+  };
 
   constructor(props){
     super(props);
@@ -30,13 +28,13 @@ export default class PdfView extends Component {
   goToNextPage = () =>
     this.setState(state => ({ pageNumber: state.pageNumber + 1 }));
 
+
   render() {
-    // const {activeNote} = this.props
-    const { pageNumber, numPages, docUrl, activeNote } = this.state;
+    const {file} = this.props
+    const { pageNumber, numPages } = this.state;
 
     return (
       <div>
-        <script crossorigin src="...">
         <nav>
           <button onClick={this.goToPrevPage}>Prev</button>
           <button onClick={this.goToNextPage}>Next</button>
@@ -44,11 +42,8 @@ export default class PdfView extends Component {
 
         <div style={{ width: 600 }}>
           <Document
-            // file = {{url: 'https://firebasestorage.googleapis.com/v0/b/reactsearch-d67f0.appspot.com/o/sample.pdf?alt=media&token=f030599c-0615-463c-803a-5e2057a2d518'}}
-            file={{url: 'https://firebasestorage.googleapis.com/v0/b/reactsearch-d67f0.appspot.com/o/chat%2Fpublic%2Fsample.pdf?alt=media&token=8571da8d-c97e-49b3-8842-da96d13608c1'}}
-            // file = {{url: "https://firebasestorage.googleapis.com/v0/b/reactsearch-d67f0.appspot.com/o/chat%2Fpublic%2F684df0a1-9d3d-49be-9e89-3d2ddfd905cb.jpg?alt=media&token=e908b15d-f3ef-47c3-91cb-15548f25e31f"} }
-            // file= {sample}
-            
+          
+            file = {file}
             onLoadSuccess={this.onDocumentLoadSuccess}
           >
             <Page pageNumber={pageNumber} width={600} />
@@ -58,7 +53,6 @@ export default class PdfView extends Component {
         <p>
           Page {pageNumber} of {numPages}
         </p>
-        </script>
       </div>
     );
   }
